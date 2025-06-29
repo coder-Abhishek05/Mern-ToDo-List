b16b13868384a51464ed33630945767f5f98654f
coder-Abhishek05 <ahsesng05@gmail.com.com>
2025-06-29 18:27:30 +0530

    Fix SPA routing: copy _redirects using vite-plugin-static-copy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.' // Places the file at the root of dist/
+        }
+      ]
+    })
+  ]
+});
+
