173305ebdcc1944654b7921c3e8a3e73d62ae192
coder-Abhishek05 <144121263+coder-Abhishek05@users.noreply.github.com>
2025-07-09 22:44:26 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,47 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     viteStaticCopy({
+//       targets: [
+//         {
+//           src: 'public/_redirects',
+//           dest: ''
+//         }
+//       ]
+//     })
+//   ]
+// });
+
+
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: ''
-        }
-      ]
-    })
-  ]
-});
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  
+  base: '/',
+  assetsInclude: ['**/*.html'],
+  
+  server: {
+    historyApiFallback: true,
+  },
+})
+
 
